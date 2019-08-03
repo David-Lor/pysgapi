1d7196889fd7707b6c0c037e8a85ff2ed05ecb49
David-Lor <17401854+david-lor@users.noreply.github.com>
2019-08-03 19:58:14 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="pysgapi",
+    version="0.0.1",
+    description="Unofficial Steamgifts API-like",
+    author="David Lorenzo",
+    url="https://github.com/David-Lor/pysgapi",
+    packages=("pysgapi",),
+    install_requires=("requests-html",)
+)
